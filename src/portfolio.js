66e714980b61b6248103adf03eda051e985a1c6e
jeetendra reddy <jeetendrareddy76@gmail.com>
2021-11-01 13:58:29 +0530

    Changes had done in the file

--- src/portfolio.js
+++ src/portfolio.js
@@ -21,7 +21,7 @@ const seo = {
 const greeting = {
   title: "Jeetendra reddy",
   logo_name: "jeethendra reddy",
-  nickname: "jithu",
+  //nickname: "jithu",
   subTitle:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   resumeLink:
